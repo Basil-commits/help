b3f20257ce8e204a779d31c233a2d3e1d3857d11
Basil-commits <nbasile617@gmail.com>
2025-07-18 12:54:12 -0400

    Updated Psyop.tsx

--- src/components/Psyop.tsx
+++ src/components/Psyop.tsx
@@ -5,7 +5,7 @@ const ImageOverlay = () => {
     <img 
       src="/MainPsyop.png" 
       alt="ZEGE Logo" 
-      className="absolute top-20 right-8 w-48 h-48 object-contain opacity-50 z-50 pointer-events-none"
+      className="fixed top-20 right-8 w-48 h-48 object-contain z-50 pointer-events-none"
     />
   );
 };
