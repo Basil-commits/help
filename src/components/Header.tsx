6d303d8d2ba3b5204117e99a187aa1ff127dbd22
Basil-commits <nbasile617@gmail.com>
2025-07-18 23:55:10 -0400

    Swap menu items and update links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,8 @@ const Header: React.FC = () => {
 
   const menuItems = [
     { name: 'About', href: '#about', isExternal: false },
+    { name: 'How to Buy', href: '#about', isExternal: false },
     { name: 'Buy', href: 'https://www.pump.fun', isExternal: true },
-    { name: 'Roadmap', href: '#roadmap', isExternal: false },
     { name: 'Community', href: 'https://www.x.com/zegecoin_', isExternal: true }
   ];
 
